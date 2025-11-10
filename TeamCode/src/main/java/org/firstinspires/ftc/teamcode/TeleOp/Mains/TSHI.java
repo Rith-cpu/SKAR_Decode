5ebc3ca2c66037b2671bed6f3ba9295d823314ee
Rith-cpu <rninganola@gmail.com>
2025-11-09 19:42:06 -0700

    Update TSHI.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/Mains/TSHI.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/Mains/TSHI.java
@@ -4,10 +4,21 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
 
 public class TSHI {
     DcMotor Lshooter, Rshooter, turretMotor, intake;
     Servo lsservo, transfer;
+
+    // Boot/transfer kicker variables
+    private boolean isKicking = false;
+    private long kickStartTime = 0;
+
+    // Your servo
+    private double transferRestPos = 0.12;   // resting/home position
+    private double transferKickPos = 0.65;   // pushing forward position
+    private long transferHoldMs = 250;       // time to hold forward (ms)
 
    public TSHI(HardwareMap Init) {
        turretMotor = Init.get(DcMotor.class, "trrt");
@@ -18,10 +29,14 @@ public class TSHI {
        transfer = Init.get(Servo.class,"trans");
 
        lsservo.setPosition(0);
-       transfer.setPosition(0);
+       transfer.setPosition(0.5);
        turretMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
        turretMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
+   }
+   public double getpos(){
+       double tpos = turretMotor.getCurrentPosition();
+       return tpos;
    }
 
    public void LSActions(Gamepad gamepad) {
@@ -36,26 +51,96 @@ public class TSHI {
        }
    }
    public void IntakeandTransfer(Gamepad gamepad) {
-       if (gamepad.x) {
-           intake.setPower(-1);
+       if (gamepad.dpad_up) {
+           transfer.setPosition(0.0);
        }
-       if (gamepad.right_stick_button) {
-           transfer.setPosition(1.0);
+       if (gamepad.dpad_down){
+           transfer.setPosition(0.5);
        }
-       intake.setPower(0);
-       transfer.setPosition(0);
-   }
-   public void TurretandShooterActions(Gamepad gamepad) {
-       Lshooter.setPower(gamepad.right_stick_y);
-       Rshooter.setPower(-gamepad.right_stick_y);
 
-       if (gamepad.right_bumper) {
+       /*if (gamepad.dpad_left) {
+           transfer.setPosition(0.5);
+       }*/
+
+       if (gamepad.x){
+           intake.setPower(1);
+       }
+       
+
+       intake.setPower(0);
+   }
+   public void ShooterActions(Gamepad gamepad) {
+       Lshooter.setPower(gamepad.right_trigger);
+       Rshooter.setPower(-gamepad.right_trigger);
+
+       /*if (gamepad.right_bumper) {
            turretMotor.setPower(-0.67);
        } else if (gamepad.left_bumper) {
            turretMotor.setPower(0.67);
        } else {
            turretMotor.setPower(0);
-       }
+       }*/
 
    }
+    public void TurretJoystickControl(Gamepad gamepad) {
+        double lx = gamepad.left_stick_x;
+        double ly = gamepad.left_stick_y;
+
+        double deadzone = 0.15;
+
+        if (Math.hypot(lx, ly) > deadzone) {
+
+            double rawDeg = Math.toDegrees(Math.atan2(lx, -ly));
+            double targetDeg = (rawDeg < 0) ? rawDeg + 360 : rawDeg;
+
+            final int TICKS_PER_REV = 6090;
+            double ticksPerDeg = TICKS_PER_REV / 360.0;
+            int targetTicksMod = (int) Math.round(targetDeg * ticksPerDeg); // 0..TPR
+
+            int currentTicks = turretMotor.getCurrentPosition();
+            int currentMod = currentTicks % TICKS_PER_REV;
+            if (currentMod < 0) currentMod += TICKS_PER_REV; // normalize to 0..TPR-1
+
+            int delta = targetTicksMod - currentMod; // desired move within one rev
+            // Wrap to shortest path
+            if (delta > TICKS_PER_REV / 2) {
+                delta -= TICKS_PER_REV;
+            } else if (delta < -TICKS_PER_REV / 2) {
+                delta += TICKS_PER_REV;
+            }
+
+            int finalTarget = currentTicks + delta; // absolute encoder target
+
+            turretMotor.setTargetPosition(finalTarget);
+            turretMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            turretMotor.setPower(0.7);
+
+        } else {
+            turretMotor.setPower(0);
+        }
+    }
+    /*public void startTransferKick() {
+        if (!isKicking) {
+            transfer.setPosition(transferKickPos);
+            kickStartTime = System.currentTimeMillis();
+            isKicking = true;
+        }
+    }
+
+    public void updateTransferKick() {
+        if (isKicking) {
+            if (System.currentTimeMillis() - kickStartTime >= transferHoldMs) {
+                transfer.setPosition(transferRestPos);
+                isKicking = false;
+            }
+        }
+    }
+
+    public void TransferKick(Gamepad gamepad){
+        if (gamepad.right_trigger > 0.5) {
+            startTransferKick();
+        }
+        updateTransferKick();
+    }*/
+
 }
