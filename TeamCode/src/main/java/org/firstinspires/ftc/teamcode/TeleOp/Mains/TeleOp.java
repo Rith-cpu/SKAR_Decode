d65cddc70a3470494483b0b39dab631c68bc1a57
Rith-cpu <rninganola@gmail.com>
2025-11-17 19:22:09 -0700

    Update TeleOp.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/Mains/TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/Mains/TeleOp.java
@@ -9,6 +9,7 @@ public class TeleOP extends LinearOpMode {
     public void runOpMode() throws InterruptedException {
         TSHI turretShooterHoodIntake = new TSHI(hardwareMap);
         Drivetrain driveTrainControl = new Drivetrain(hardwareMap);
+        Limelight ll = new Limelight(hardwareMap);
         //SensorSparkFunOTOS sparkFunControl = new SensorSparkFunOTOS(hardwareMap);
         //sparkFunControl.configureOtos(telemetry);
 
@@ -29,7 +30,11 @@ public class TeleOP extends LinearOpMode {
             turretShooterHoodIntake.LSActions(gamepad2);
             turretShooterHoodIntake.ShooterActions(gamepad2);
             turretShooterHoodIntake.IntakeandTransfer(gamepad2);
-            turretShooterHoodIntake.TurretJoystickControl(gamepad2);
+            //turretShooterHoodIntake.TurretJoystickControl(gamepad2);
+            ll.setTargetId(24);
+            ll.runLL();
+            ll.setGains(0.015, 1.0,1.0);
+
 
             //sparkFunControl.OTOS(gamepad1,telemetry);
 
